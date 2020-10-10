12058d1ff31b5bfa5a2cdec2582b5a9950b87693
Kaito <kaito.neko@icloud.com>
2020-10-10 17:31:25 +1030

    Missed in last commit

--- Assets/Scripts/GameDirector.cs
+++ Assets/Scripts/GameDirector.cs
@@ -2,12 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameDirectorMain : MonoBehaviour
+public class GameDirector : MonoBehaviour
 {
+    public GameObject player;
+    public GameObject bigChongus;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int counter = 0; counter < 2; counter++)
+        {
+            Debug.Log("Creating Chongus...");
+            var newChongus = Instantiate(bigChongus, player.transform.position, Quaternion.identity) as GameObject;
+            newChongus.gameObject.SetActive(true);
+        }
     }
 
     // Update is called once per frame
