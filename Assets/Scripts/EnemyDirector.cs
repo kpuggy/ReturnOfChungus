f0e8edee5af941e103e89907a6bde5bcba19b825
Kaito <kaito.neko@icloud.com>
2020-10-10 17:31:12 +1030

    Audio working, reload working.

---
+++ Assets/Scripts/EnemyDirector.cs
@@ -0,0 +1,151 @@
+﻿
+using System.Collections;
+using UnityEngine;
+
+public class EnemyDirector : MonoBehaviour
+{
+
+    public float health = 50f;
+    public CharacterController enemyController;
+    public Transform playerTransform;
+    public Transform enemyTransform;
+    public AudioSource closeRangeAudio;
+    public AudioSource dieAudio;
+    public float closeRangeDistance = 40f;
+    public float distance;
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public bool isDead = false;
+    Vector3 velocity;
+
+    // Update is called once per frame
+    public void Update()
+    {
+        // Apply Audio for Enemy
+        Audio();
+
+        float x = 0;
+        float z = 0;
+
+
+        // Forward / Back Movement
+        if (playerTransform.position.x >= enemyTransform.position.x + 20)
+        {
+            z = 3;
+        }
+        if (playerTransform.position.x < enemyTransform.position.x + 20)
+        {
+            z = 1;
+        }
+
+        // Move enemy in player direction
+        Vector3 move = transform.right * x + transform.forward * z;
+        enemyController.Move(move * speed * Time.deltaTime);
+
+        // Rotate enemy towards player
+        Vector3 direction = playerTransform.position - enemyTransform.position;
+        Quaternion rotation = Quaternion.LookRotation(direction);
+        enemyTransform.rotation = Quaternion.Lerp(enemyTransform.rotation, rotation, speed * Time.deltaTime);
+
+        // Apply Gravity
+        velocity.y += gravity * Time.deltaTime;
+        enemyController.Move(velocity * Time.deltaTime);
+
+
+    }
+
+    // Plays, Stops or Adjust Volume for Enemy Proximity Audio
+    private void Audio()
+    {
+        if (closeRangeAudio != null && !isDead)
+        {
+            distance = Vector3.Distance(playerTransform.position, enemyTransform.position);
+
+            if (distance < closeRangeDistance && !closeRangeAudio.isPlaying)
+            {
+                closeRangeAudio.Play();
+            }
+            else if (distance > closeRangeDistance && !closeRangeAudio.isPlaying)
+            {
+                closeRangeAudio.Stop();
+            }
+
+            // Adjust Audio by distance
+            if (closeRangeAudio.isPlaying)
+            {
+                closeRangeAudio.volume = 1 - (distance / closeRangeDistance);
+            }
+        }
+    }
+
+    // Record Damage from hit
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+
+        if (health <= 0f)
+        {
+            StartCoroutine(Die());
+        }
+    }
+
+    public IEnumerator Die()
+    {
+        if (this.health <= 0f && !isDead)
+        {
+            this.isDead = true;
+
+            if (closeRangeAudio != null && closeRangeAudio.isPlaying)
+            {
+                this.closeRangeAudio.Stop();
+            }
+
+            if (this.dieAudio != null)
+            {
+                this.dieAudio.Play();
+            }
+
+
+            //gameObject.GetComponent<Renderer>().enabled = false;
+            //yield return null;
+
+            //if (renderer != null)
+            //{
+            //
+            //    
+            //    Debug.Log("Setting transparent...");
+            //    var colorToAdjust = renderer.material.color;
+            //    colorToAdjust.a = 0f;
+            //    renderer.material.color = colorToAdjust;
+            //
+            //}
+
+            float waitTime = 0;
+
+            if (dieAudio != null)
+            {
+                waitTime = dieAudio.clip.length;
+            }
+
+
+            yield return new WaitForSeconds(waitTime);
+            if (dieAudio != null)
+            {
+                this.dieAudio.Stop();
+            }
+            Destroy(gameObject);
+
+            
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
+
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,138 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class Weapon : MonoBehaviour
+{
+    // Test
+    public float damage = 10f;
+    public float range = 100f;
+    public Camera fpsCam;
+    public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
+    public float fireRate = 10f;
+    public int maxAmmo = 0;
+    public int currentAmmo;
+    public float reloadTime = 2f;
+    public Animator animator;
+    public bool reloadingNow = false;
+
+
+    private float nextTimeToFire = 0f;
+    // Update is called once per frame
+
+    public void Start()
+    {
+        // Reload current ammo to maxAmmo
+        StartCoroutine(Reload(false));
+    }
+
+
+    void Update()
+    {
+        // Check if gun has been fired
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        {
+            // Check if gun has ammo
+            if (currentAmmo > 0)
+            {
+                nextTimeToFire = Time.time + 1f / fireRate;
+                StartCoroutine(Shoot());
+            }
+            else
+            {
+                StartCoroutine(Reload());
+            }
+
+        }
+
+        //if (Input.GetButton("Reload"))
+        //{
+        //    Reload();
+        //}
+    }
+
+    private IEnumerator Reload(bool showAnimation = true)
+    {
+
+        if (!showAnimation)
+        {
+            currentAmmo = maxAmmo;
+        }
+
+        if (!reloadingNow && showAnimation)
+        {
+            reloadingNow = true;
+
+            Debug.Log("Reloading...");
+
+            if (animator != null)
+            {
+                animator.SetBool("isReloading", true);
+            }
+
+            // RELOAD.Play();
+            yield return new WaitForSeconds(reloadTime - .25f);
+
+            if (animator != null)
+            {
+                animator.SetBool("isReloading", false);
+            }
+            yield return new WaitForSeconds(.50f);
+            currentAmmo = maxAmmo;
+
+            reloadingNow = false;
+            Debug.Log("Reload Complete...");
+
+        }
+    }
+
+
+    private IEnumerator Shoot()
+    {
+        RaycastHit hit;
+
+
+        // Reduce ammo by 1
+        currentAmmo--;
+
+        // Play Muzzle Animation
+        if (muzzleFlash != null)
+        {
+            muzzleFlash.Play();
+        }
+
+        // Trigger Recoil Animation
+        if (animator != null)
+        {
+            animator.SetBool("isRecoil", true);
+
+        }
+
+        // Check if Raycast detects hit
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            EnemyDirector target = hit.transform.GetComponent<EnemyDirector>();
+
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+                if (impactEffect != null)
+                {
+                    Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+                }
+            }
+
+        }
+
+        // Come back once the recoil animation is done and next fire available
+        yield return new WaitForSeconds(1f / fireRate);
+        if (animator != null)
+        {
+            animator.SetBool("isRecoil", false);
+        }
+    }
+}
+
